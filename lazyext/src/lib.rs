753298259e77be5e7e49e3daf97cdd2c7277fe76
al8n <scygliu1@gmail.com>
2022-01-17 00:36:43 +0800

    [cargo release] 0.0.3

--- lazyext/src/lib.rs
+++ lazyext/src/lib.rs
@@ -1,12 +1,12 @@
 //! Tons of extension utility functions for Rust.
 //!
 #![cfg_attr(not(feature = "std"), no_std)]
-#![doc(html_root_url = "https://docs.rs/lazyext/0.0.2")]
+#![doc(html_root_url = "https://docs.rs/lazyext/0.0.3")]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, allow(unused_attributes))]
 #![deny(missing_docs)]
 
-pub use lazyext_bytes as slice_ext;
+pub use lazyext_slice as slice_ext;
 
 /// util macros
 #[cfg(feature = "lazyext-macros")]
